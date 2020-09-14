57a9b79b3a34ae928468cd9a5d8e7ed51bb08cd3
ersin <ersinclair@gmail.com>
2020-09-14 14:29:12 -0700

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,47 @@
+﻿/// Homework No. 2		Exercise Part 1
+/// File Name: PigLatin
+/// @author: Evan Sinclair
+/// Date:  Sept 14, 2020
+///
+/// Problem Statement: Change a name into Pig Latin and capitalize it
+/// 
+/// 
+/// Overall Plan:
+/// 1)save the first letter in each name
+/// 2)create pigliatin names using char.ToUpper() on the second character in the name + substring from the third position + first letter + "ay"
+/// 3)Output pig latin names
+///
+///
+
+
+
+
+using System;
+
+
+using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "evan";
+            string secondName = "sinclair";
+
+            
+            // process first name
+            string firstLetter = firstName.Substring(0, 1);
+            string pigFirst = char.ToUpper(firstName[1]) + firstName.Substring(2) + firstLetter + "ay";
+
+            // process second name
+            firstLetter = secondName.Substring(0, 1);
+            string pigSecond = char.ToUpper(secondName[1]) + secondName.Substring(2) + firstLetter + "ay";
+
+
+            Console.WriteLine(pigFirst + " " + pigSecond);
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,43 @@
+﻿/// Homework No. 2		Exercise Part 2
+/// File Name: FahrenheitToCelsius
+/// @author: Evan Sinclair
+/// Date:  Sept 14, 2020
+///
+/// Problem Statement: Turn a temperature from Fahrenheit to Celsius
+/// 
+/// 
+/// Overall Plan:
+/// 1)Prompt user for a Temperature and store as a double
+/// 2)Use equation to convert into Celsius
+/// 3)Round temperature using Math.Round()
+/// 4)Output converted temperature
+///
+///
+
+
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Prompt user for a temp in F
+            Console.WriteLine("Enter a temperature in degrees Fahrenheit");
+            
+            // Read input and convert to double
+            double fTemp = Convert.ToDouble(Console.ReadLine());
+            
+            // Calculate temp in C
+            double cTemp = (fTemp - 32) * 5 / 9;
+
+            // Round
+            cTemp = Math.Round(cTemp, 2);
+
+            // Output information
+            Console.WriteLine(fTemp + " degrees Fahrenheit is equal to " + cTemp + " degrees Celsius ");
+        }
+    }
+}
